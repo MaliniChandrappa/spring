1c929e63bcff0778166d70d431219e93c2252bcd
MaliniChandrappa <50769169+MaliniChandrappa@users.noreply.github.com>
2019-05-19 16:03:27 +0530

    Update ResponseServlet.java

--- ipl_team_management/src/com/xworkz/ipl/servlet/ResponseServlet.java
+++ ipl_team_management/src/com/xworkz/ipl/servlet/ResponseServlet.java
@@ -1,7 +1,7 @@
 package com.xworkz.ipl.servlet;
 
 import java.io.IOException;
-
+//hi
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
